15899fe8a4bebbe6f58894ab2693beebe72ba7be
Everson Calabazas <everson.esc@gmail.com.br>
2020-09-22 13:19:12 -0400

    CRUD Funcional - Segurança

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,9 @@ Route::group(['middleware' => 'web'], function(){
     Route::get('/home', 'HomeController@index')->name('home'); 
 });
 
-Route::get('/usuarios', 'UsuariosController@index');
-Route::get('/usuarios/new', 'UsuariosController@new');
-Route::post('usuarios/add', 'UsuariosController@add');
-Route::get('usuarios/{id}/edit', 'UsuariosController@edit');
-Route::post('usuarios/update/{id}', 'UsuariosController@update');
-Route::delete('usuarios/delete/{id}', 'UsuariosController@delete');
+Route::get('/usuarios', 'UsuariosController@index')->middleware('auth');
+Route::get('/usuarios/new', 'UsuariosController@new')->middleware('auth');
+Route::post('usuarios/add', 'UsuariosController@add')->middleware('auth');
+Route::get('usuarios/{id}/edit', 'UsuariosController@edit')->middleware('auth');
+Route::post('usuarios/update/{id}', 'UsuariosController@update')->middleware('auth');
+Route::delete('usuarios/delete/{id}', 'UsuariosController@delete')->middleware('auth');
